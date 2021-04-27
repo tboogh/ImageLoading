e32a6ab361b05b68b7f5b32d3811f74bc3830ea2
Tobias Boogh <t@boogh.se>
2021-04-27 19:04:26 +0200

    Initial Commit

---
+++ Shared/NasaImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaImagesApp.swift
+//  Shared
+//
+//  Created by Tobias Boogh on 2021-04-27.
+//
+
+import SwiftUI
+
+@main
+struct NasaImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
